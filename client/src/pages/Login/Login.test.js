987457ad2557ad10c8906ca4607e776a0d588f7c
tzyan35 <zhongyan1997@hotmail.com>
2022-12-03 16:08:19 +0800

    login testing

---
+++ client/src/pages/Login/Login.test.js
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/react';
+import Login from './Login';
+
+test("username input should be rendered", () => {
+    render(<Login />);
+    const usernameInputEl = screen.getByPlaceholderText(/username/i);
+    expect(usernameInputEl).toBeInTheDocument();
+  });
+
+  test("password input should be rendered", () => {
+    render(<Login />);
+    const passwordInputEl = screen.getByPlaceholderText(/password/i);
+    expect(passwordInputEl).toBeInTheDocument();
+  });
+
+  test("button should be rendered", () => {
+    render(<Login />);
+    const buttonEl = screen.getByRole("button");
+    expect(buttonEl).toBeInTheDocument();
+  });
+
+  test("username input should be empty", () => {
+    render(<Login />);
+    const usernameInputEl = screen.getByPlaceholderText(/username/i);
+    expect(usernameInputEl.value).toBe("");
+  });
+  
+  test("password input should be empty", () => {
+    render(<Login />);
+    const passwordInputEl = screen.getByPlaceholderText(/password/i);
+    expect(passwordInputEl.value).toBe("");
+  });
