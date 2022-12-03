75619ed669cd0afaf708979873b988b3a3c234af
tzyan35 <zhongyan1997@hotmail.com>
2022-12-03 11:15:02 +0800

    added button in login

--- client/src/context/authContext.jsx
+++ client/src/context/authContext.jsx
@@ -4,10 +4,30 @@ import { createContext, useEffect, useState } from "react";
 export const AuthContext = createContext();
 
 const AuthContextProvider = ({children}) => {
+
+    const [currentUser, setCurrentUser] = useState(
+        JSON.parse(localStorage.getItem("user")) || null
+      );
+
+      
+  const login = async (inputs) => {
+    const res = await axios.post("", inputs);
+    setCurrentUser(res.data);
+  };
+
+  const logout = async (inputs) => {
+    await axios.post("");
+    setCurrentUser(null);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(currentUser));
+  }, [currentUser]);
+
+  console.log(currentUser)
     return (
         <AuthContext.Provider value={{ currentUser, login, logout }}>
           {children}
         </AuthContext.Provider>
       );
     };
-}
