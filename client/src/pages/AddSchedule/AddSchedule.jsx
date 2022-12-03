63eaf2f99fadf04c434fcb93d6f78089a4357350
tzyan35 <zhongyan1997@hotmail.com>
2022-12-03 14:52:51 +0800

    create add schedule functionality

--- client/src/pages/AddSchedule/AddSchedule.jsx
+++ client/src/pages/AddSchedule/AddSchedule.jsx
@@ -1,8 +1,78 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+
 
 const AddSchedule = () => {
+    const [schedule, setSchedule] = useState({
+        TransactionID: "",
+        AccountID: "",
+        ReceivingAccountID: "",
+        Date: "",
+        TransactionAmount: "",
+        Comment: ""
+      });
+   
+const handleChange = (e) => {
+    setSchedule((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+}
+
+const handleSubmit = async (e) => {
+    e.preventDefault()
+    const res = await axios.post('', schedule)
+}
+
+console.log(schedule)
   return (
-    <div>AddSchedule</div>
+    <div>
+        <Navbar/>
+        <h1>Schedule a transaction</h1>
+     <form>
+     <input
+          required
+          type="int"
+          placeholder="TransactionID"
+          name="TransactionID"
+          onChange={handleChange}
+        />
+             <input
+          required
+          type="int"
+          placeholder="AccountID"
+          name="AccountID"
+          onChange={handleChange}
+        />
+                     <input
+          required
+          type="int"
+          placeholder="ReceivingAccountID"
+          name="ReceivingAccountID"
+          onChange={handleChange}
+        />
+                     <input
+          required
+          type="int"
+          placeholder="Date"
+          name="Date"
+          onChange={handleChange}
+        />
+                             <input
+          required
+          type="int"
+          placeholder="TransactionAmount"
+          name="TransactionAmount"
+          onChange={handleChange}
+        />
+                             <input
+          required
+          type="int"
+          placeholder="Comment"
+          name="Comment"
+          onChange={handleChange}
+        />
+     </form>
+     <button onChange={handleSubmit}>Set Transaction</button>
+    </div>
   )
 }
 
