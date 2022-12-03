824edbb1a5ed6d28f68a1885ac7d76bad223a034
tzyan35 <zhongyan1997@hotmail.com>
2022-12-03 11:09:35 +0800

    added login functionality

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Login = () => {
+
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+  });
+
+  const [error, setError] = useState("")
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  console.log(inputs)
+
   return (
-    <div>Login</div>
+    <div>
+      <form>
+        <input required type="text" placeholder='username' name='username' onchange={handleChange}/>
+        <input required type="password" placeholder='password' name='password' onchange={handleChange}/>
+      </form>
+      <button>Login</button>
+      Login hello
+      </div>
   )
 }
 
