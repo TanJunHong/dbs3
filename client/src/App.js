716d524176abf6bd65fec0125081be8ed6d2577a
tzyan35 <zhongyan1997@hotmail.com>
2022-12-03 10:55:41 +0800

    created update and schedule

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './pages/Login/Login';
+import Home from './pages/Home/Home';
+import Update from './pages/Update/Update';
+import Schedule from './pages/Schedule/Schedule';
 
 function App() {
   return (
     <div className="App">
-    
+    <BrowserRouter>
+    <Routes>
+      <Route strict exact path={'/login'} element={<Login/>}></Route>
+      <Route strict exact path={'/'} element={<Home/>}></Route>
+      <Route strict exact path={'/update'} element={<Update/>}></Route>
+      <Route strict exact path={'/schedule'} element={<Schedule/>}></Route>
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
