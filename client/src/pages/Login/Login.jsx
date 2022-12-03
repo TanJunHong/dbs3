34a77ed5c7d920abe18ef179a3ab58bffe341a46
junhong <b190013@e.ntu.edu.sg>
2022-12-03 14:59:07 +0800

    Fix login

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,81 +1,55 @@
-import { Button, Container, TextField } from "@material-ui/core";
-import React, { useContext } from "react";
-import { useState } from "react";
+import {Button, Container, TextField} from "@material-ui/core";
+import React, {useContext} from "react";
+import { useState } from 'react'
 import { useNavigate } from "react-router-dom";
-import "./login.css";
-import { AuthContext } from "../../context/authContext";
+import './login.css'
+import {AuthContext} from "../../context/authContext";
+
 
 const Login = () => {
-  const [err, setError] = useState(null);
-  const [schedule, setSchedule] = useState({
-    TransactionID: "",
-    AccountID: "",
-    ReceivingAccountID: "",
-    Date: "",
-    TransactionAmount: "",
-    Comment: "",
+  const [err, setError] = useState(null)
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
   });
 
-  const { currentUser } = useContext(AuthContext);
-
+  const { login } = useContext(AuthContext);
+  const navigate = useNavigate()
 
   const handleChange = (e) => {
-    setSchedule((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
 
-  const handleSubmit = async (e) => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const formData = new FormData();
+    formData.append("username", inputs.username);
+    formData.append("password", inputs.password);
+    await login(inputs);
+  };
 
   return (
-    <>
       <div>
         <h1>Login</h1>
-        <form>
-          <input
-            required
-            type="int"
-            placeholder="TransactionID"
-            name="TransactionID"
-            onChange={handleChange}
-          />
-          <input
-            required
-            type="int"
-            placeholder="AccountID"
-            name="AccountID"
-            onChange={handleChange}
-          />
-          <input
-            required
-            type="int"
-            placeholder="ReceivingAccountID"
-            name="ReceivingAccountID"
-            onChange={handleChange}
-          />
-          <input
-            required
-            type="int"
-            placeholder="Date"
-            name="Date"
-            onChange={handleChange}
-          />
-          <input
-            required
-            type="int"
-            placeholder="TransactionAmount"
-            name="TransactionAmount"
-            onChange={handleChange}
-          />
-          <input
-            required
-            type="int"
-            placeholder="Comment"
-            name="Comment"
-            onChange={handleChange}
-          />
-        </form>
+        <Container component="main" maxWidth="xs">
+          <form onSubmit={handleSubmit}>
+            <TextField type="text" variant="outlined" margin="normal" required fullWidth id="username"
+                       inputProps={{title: "username"}}
+                       label="Username" name="username" autoComplete="username" autoFocus
+                       onChange={handleChange}/>
+            <TextField variant="outlined" margin="normal" required fullWidth name="password" label="Password"
+                       inputProps={{title: "password"}}
+                       type="password" id="password" autoComplete="current-password"
+                       onChange={handleChange}/>
+            <Button type="submit" fullWidth variant="contained" color="primary"
+                    title="sign-in">
+              Sign In
+            </Button>
+          </form>
+        </Container>
       </div>
-    </>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
